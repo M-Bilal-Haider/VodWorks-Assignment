40e1318ccfa32e20f982a968c8deb4c2c182757a
Bilal Haider <bilal.haider@webrammers.com>
2021-09-26 20:32:50 +0500

    Initial Commit

---
+++ VodWorks Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VodWorks Assignment
+//
+//  Created by Bilal haider on 9/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VodWorks AssignmentTests/VodWorks_AssignmentTests.swift
@@ -0,0 +1,33 @@
+//
+//  VodWorks_AssignmentTests.swift
+//  VodWorks AssignmentTests
+//
+//  Created by Bilal haider on 9/26/21.
+//
+
+import XCTest
+@testable import VodWorks_Assignment
+
+class VodWorks_AssignmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
